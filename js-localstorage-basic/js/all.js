3f5eebf91a5cf85494f0192f44e09b5d4839c97a
謝秉錡 <cookie_seventeen@msn.com>
2019-05-18 14:29:21 +0800

    加入gulp 起手式

---
+++ js-localstorage-basic/js/all.js
@@ -0,0 +1,12 @@
+
+var str = 'tom';
+localStorage.setItem("myName", str);
+console.log(localStorage.getItem('myName'));
+
+var benishand="it's true";
+localStorage.setItem("benishansome", benishand);
+
+_output=localStorage.getItem('benishansome');
+console.log('_output');
+
+
