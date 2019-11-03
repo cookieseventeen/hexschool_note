438e3a29e3f3228dae36b9e74aa3de0acffc9d10
謝秉錡 <cookie_seventeen@msn.com>
2019-11-03 23:09:30 +0800

    筆記第四章

--- Chapter_4_template/js/all.js
+++ Chapter_4_template/js/all.js
@@ -1,132 +1,110 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyD5mArY-z7jkxYbaXNYzpkR2RbY5hJvDy4",
-    authDomain: "our-to-do-list-ce3b5.firebaseapp.com",
-    databaseURL: "https://our-to-do-list-ce3b5.firebaseio.com",
-    projectId: "our-to-do-list-ce3b5",
-    storageBucket: "our-to-do-list-ce3b5.appspot.com",
-    messagingSenderId: "246151140434",
-    appId: "1:246151140434:web:48b833ac03de3e55"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var firebasedb = firebase.database();
-/*
-var initdata;
-firebasedb
-    .ref("tododata")
-    .on('value', function (datacontent) {
-        initdata = datacontent.val();
-    });
-*/
-var vue_todolist = new Vue({
-    el: '#vue_todolist',
-    data: {
-        todotips: '記載所有todo list,請再輸入匡輸入你要記錄的todo內容,記得按下儲存按鈕.',
-        todoinput: '',
-        todoitem: '',
-        //resultoutput: '',
-        degree: '1',
-        executor: 'Ben',
-        listfilter: 'all',
-        cachetodo:'',
-        cachetodocontent:''
+Vue.component('list-item', {
+    data: function () {
+        return {counter: 0}
     },
-    methods: { //用來定義Ｖue 實體內使用的函數
-        addlist: function (tododata) {
+    template: `<li >
+        {{item.name}} {{item.age}} 歲 <input></input>
+    </li>`,
+    props:['item']
+});
 
-            if (!tododata.trim()) {
-                alert('請輸入內容');
-                return;
+var vue_todolist = new Vue({
+    el: '#vue_template_demo',
+    data: {
+        vmodetext: '',
+        vhtmldemo: '<p class="imvhtml">我是vhtml的測試字串<p>',
+        voncedata: 'once',
+        nember_1: '11',
+        nember_2: '22',
+        bindid: 'thisisvbind',
+        disabledswitch: false,
+        filtertext:'',
+        objclass: {
+            'rotate': false,
+            'background-danger': false
+        },
+        arrayclass: [],
+        inlinecssdemo: {
+            backgroundColor: '#000',
+            borderColor: 'red',
+            borderWidth: '10px',
+            borderboderStyle: 'solid'
+        },
+        inlinecssdemo2: {
+            boxShadow: '2px 2px 2px 2px rgba(100,100,100,0.6)'
+        },
+        vuefordemo: {
+            Ben: {
+                name: '班',
+                age: '32歲'
+            },
+            Mary: {
+                name: '馬力',
+                age: '33歲'
+            }
+        },
+        arrayvuefordemo: [
+            {
+                name: '班',
+                age: '32歲'
+            }, {
+                name: '馬力',
+                age: '33歲'
             }
 
-            var time = (new Date()).valueOf();
-            var data = {
-                "time": time,
-                "content": this.todoinput,
-                "degree": this.degree,
-                "executor": this.executor,
-                "progress_rate": "0%",
-                "status": "pause",
-                "complated": false
-            }
-
-            firebasedb
-                .ref("tododata")
-                .push(data);
-            console.log(data);
-
-            this.todoinput = '';
+        ],
+        filterarray:[],
+    },
+    methods: {
+        reverseObj: function () {
+            console.log(this.arrayvuefordemo);
+            this.arrayvuefordemo.reverse();
+            console.log('reverse');
         },
-        delete_target: function (target) {
-            var delete_target = target;
-
-            firebasedb
-                .ref("tododata/" + delete_target)
-                .remove();
-            console.log(delete_target);
+        filterdata: function(){
+            var vm=this;//原本不解為什麼要特別弄一個this,出來,後來才發現,如果你有其他或函式可能也會用到this,那先宣告一個this就很重要;
+            vm.filterarray=vm.arrayvuefordemo.filter(function(item){
+                console.log(vm.filtertext,item.name,item.name.match(vm.filtertext));
+                return item.name.match(vm.filtertext)
+            });
 
         },
-        complated_target: function (target) {
-            console.log(target);
-            /*console.log(this.todoitem);*/
-            //console.log(firebasedb.ref("tododata/" + target+'/complated'));
+        cantwork:function(){
+            console.log('cantwork');
+            //this.arrayvuefordemo.length=0;
+            //console.log('過去在一般狀態下寫ＪＳ 可能可以用這種方式將東西清除,但是在這裡不行這樣操作,就算你這樣操作,DOM也不會這樣改變');
+            //console.log(this.arrayvuefordemo)
+
+            //如果你直接更改陣列裡面的物件,其實他是可以直接更新資料的,包含前端的輸出
+            //可是如果你是更改物件,他其實是不會動作的,必續透過set 才會重新渲染資料.
             
-            firebasedb
-                .ref("tododata/" + target + '/complated')
-                .once('value', function (snapshot) {
-                    console.log('status:' + snapshot.val());
+            //透過陣列更改資料可以改值 並且會重新更新前端的資料
+            //this.arrayvuefordemo[0].name="班班";
+            //this.arrayvuefordemo[0].age="100歲";
+            
 
-                    if (snapshot.val()) {
-                        firebasedb
-                            .ref("tododata/" + target + '/complated')
-                            .set(false);
-                    } else {
-                        firebasedb
-                            .ref("tododata/" + target + '/complated')
-                            .set(true);
-                    }
+            //直接更新整個物件,並不會重新渲染前端,就像這個依樣
+            /*this.arrayvuefordemo[0]={
+                name: '100班',
+                age: '100歲'
+            };*/
 
-                });
-            /*
-            firebasedb
-                .ref("tododata")
-                .once('value', function (datacontent) {
-                    this.todoitem = datacontent.val();
-                });*/
+            //透過vue set 的方式更新資料
 
-        },
-        edittodo: function(target,content){
-            this.cachetodo=target;
-            this.cachetodocontent=content;
-        },
-        leaveedit: function(){
-            console.log();
-            this.cachetodo='';
-            this.cachetodocontent='';
-        },
-        updatetodo:function(){
-            firebasedb.ref("tododata/" + this.cachetodo + '/content').set(this.cachetodocontent);
-            this.cachetodo='';
-            this.cachetodocontent='';
-        },
-        clear:function(){
-            firebasedb.ref("tododata/").set('');
+            Vue.set(this.arrayvuefordemo,0,{
+                name: '100班',
+                age: '100歲'
+            });
+
         }
     },
-    mounted: function () { //初始執行的程式 也可以縮寫成mounted() {} mounted英文翻譯 已安裝
-        var this_vue = this;
-        firebasedb
-            .ref("tododata")
-            .on('value', function (datacontent) {
-                this_vue.todoitem = datacontent.val();
-            });
+    mounted: function () {
         console.log('mounted');
     },
     created: function () {
         console.log('created');
     },
+    /*
     computed: { //computed 比較像一個監聽動態的執行函式,如果有觸擊的事件的話不必用這個,使用methods 會比較恰當,另外我們還有一個物件叫做watch,另外在做比較
         reversetext: function () {
             console.log('reversetext');
@@ -180,29 +158,10 @@ var vue_todolist = new Vue({
         }
 
     }
+    */
 });
 
-Vue.component('counter-component', {
-    data: function () {
-        return {counter: 0}
-    },
-    template: '<div class="counter"> <button @click="counter +=1" class="btn btn-sm">{{counte' +
-            'r}}</button></div>'
-});
 
-var counter_component = new Vue({
-    el: '#counter_demo',
-    data: {
-        counter: 0
-    }
-});
-
-var toogledemo=new Vue({
-    el: '#toogle_demo',
-    data:{
-        toogleclassswitch: false,
-    }
-})
 
 /* Methods 與 Computed 的使用情境
 - computed 是在監控資料更動後，重新運算結果呈現於畫面上
